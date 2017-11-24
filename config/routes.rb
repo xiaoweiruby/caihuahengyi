095f051474dd852bdda4044e57ff0081655353f0
Zhang-jia-chen <zjc834723074@gmail.com>
2017-11-24 11:01:12 +0800

    add comment

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :posts
   end # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "welcome#index"
+  root "posts#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171123083237) do
+ActiveRecord::Schema.define(version: 20171124022543) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
