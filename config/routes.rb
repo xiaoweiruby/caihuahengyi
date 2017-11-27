8eddf105d043e52c3e589c6e0fbc998f01be2ed7
Zhang-jia-chen <zjc834723074@gmail.com>
2017-11-27 18:18:06 +0800

    add comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   namespace :admin do
     resources :posts
   end # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
