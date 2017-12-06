23d183823c2d2d0005f1d658fb112ad0b223f298
Zhang-jia-chen <zjc834723074@gmail.com>
2017-12-06 13:57:53 +0800

    add price add_column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205024931) do
+ActiveRecord::Schema.define(version: 20171206055213) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20171205024931) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "image"
+    t.integer  "price"
   end
 
   create_table "users", force: :cascade do |t|
