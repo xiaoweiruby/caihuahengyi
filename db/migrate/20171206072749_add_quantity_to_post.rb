61e8dd1cbadf569c5f227edb58c46b75b20e1f4b
Zhang-jia-chen <zjc834723074@gmail.com>
2017-12-06 15:53:18 +0800

    更新的数量不能超过库存

--- db/migrate/20171206072749_add_quantity_to_post.rb
+++ db/migrate/20171206072749_add_quantity_to_post.rb
@@ -1,5 +1,5 @@
 class AddQuantityToPost < ActiveRecord::Migration[5.0]
   def change
-    add_column :posts, :quantity, :integer, default: 1
+    add_column :posts, :quantity, :integer, default: 100
   end
 end
