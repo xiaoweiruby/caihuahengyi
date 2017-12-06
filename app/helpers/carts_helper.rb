97732017a85ab91c97aaf65c3934724f73687387
Zhang-jia-chen <zjc834723074@gmail.com>
2017-12-06 14:15:15 +0800

    debug

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,2 +1,11 @@
 module CartsHelper
+  def render_cart_total_price(cart)
+    sum = 0
+        current_cart.cart_items.each do |cart_item|
+        if cart_item.post.price.present?
+        sum = sum + cart_item.quantity * cart_item.post.price
+        end
+    end
+      sum
+  end
 end
