e98b4b3f3eacf2e8aaa3913f83719c3ba6126c78
Zhang-jia-chen <zjc834723074@gmail.com>
2017-12-13 09:53:02 +0800

    change postlists#create

--- app/controllers/postlists_controller.rb
+++ app/controllers/postlists_controller.rb
@@ -15,7 +15,7 @@ class PostlistsController < ApplicationController
     @postlist.post = @post
     @postlist.user = current_user
     if @postlist.save
-      redirect_to post_postlist_path(@post)
+      redirect_to post_path(@post)
     else
       render :new
     end
@@ -29,7 +29,5 @@ class PostlistsController < ApplicationController
   private
   def postlist_params
     params.require(:postlist).permit(:title, :body)
-
-
   end
 end
