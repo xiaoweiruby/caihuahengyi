4f9a9d4078f130069734b9c2aaae58413263e9c5
Zhang-jia-chen <zjc834723074@gmail.com>
2017-12-17 18:56:13 +0800

    add qa

--- app/controllers/qas_controller.rb
+++ app/controllers/qas_controller.rb
@@ -1,5 +1,28 @@
 class QasController < ApplicationController
   def index
+    @qas = Qa.all
+  end
+  def new
+    @qa = Qa.new
+  end
+  def create
+    @qa.new(qa_params)
+    if @qa.save
+      redirect_to qas_path
+    else
+      render :new
+    end
+  end
+  def show
+    @qa = Qa.find(params[:id])
+  end
+  def edit
+    @qa = Qa.find(params[:id])
 
+  end
+
+  private
+  def qa_params
+    params.require(:qa).permit(:title, :body)
   end
 end
