15e70359e7c96496815883743d9468b989528024
Zhang-jia-chen <zjc834723074@gmail.com>
2017-12-18 15:00:15 +0800

    finish  activity crud

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,6 @@
 class ActivitiesController < ApplicationController
   def index
-
+    @activities = Activity.all
   end
   def new
     @activity = Activity.new
@@ -8,9 +8,31 @@ class ActivitiesController < ApplicationController
   end
   def create
     @activity = Activity.new(activity_params)
-    redirect_to activities_path
-    
+    if @activity.save
+      redirect_to activities_path
+    else
+      rendet :new
+    end
+  end
+  def show
+    @activity = Activity.find(params[:id])
 
+  end
+  def edit
+    @activity = Activity.find(params[:id])
+  end
+  def update
+    @activity = Activity.find(params[:id])
+    if @activity.update(activity_params)
+      redirect_to activities_path
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @activity = Activity.find(params[:id])
+    @activity.destroy
+    redirect_to activities_path
   end
   private
   def activity_params
