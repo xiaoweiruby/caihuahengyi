e1f5ecf9f09cf66c12b204d359d2c47b3e11c591
Zhang-jia-chen <zjc834723074@gmail.com>
2017-12-18 11:29:38 +0800

    implement activities#create

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,4 +2,19 @@ class ActivitiesController < ApplicationController
   def index
 
   end
+  def new
+    @activity = Activity.new
+
+  end
+  def create
+    @activity = Activity.new(activity_params)
+    redirect_to activities_path
+    
+
+  end
+  private
+  def activity_params
+    params.require(:activity).permit(:title, :body)
+
+  end
 end
